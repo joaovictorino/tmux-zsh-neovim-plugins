b19325f0fe1fc58cfb92dce9b1df73d55bad8441
João Victorino <jhvictorino@gmail.com>
2023-04-28 16:10:34 -0300

    Update nvim-tree.lua

--- .config/nvim/lua/joao/plugins/nvim-tree.lua
+++ .config/nvim/lua/joao/plugins/nvim-tree.lua
@@ -15,6 +15,20 @@ vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 nvimtree.setup({
   -- change folder arrow icons
   renderer = {
+    indent_markers = {
+      enable = true,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        item = "│ ",
+        none = "  ",
+      },
+    },
+    update_focused_file = {
+      enable = true,
+      update_cwd = false,
+      ignore_list = {},
+    },
     icons = {
       glyphs = {
         folder = {
@@ -34,6 +48,9 @@ nvimtree.setup({
       },
     },
   },
+  view = {
+    adaptive_size = true,
+  },
   on_attach = on_attach,
 })
 
@@ -49,7 +66,6 @@ local function on_attach(bufnr)
   vim.keymap.set('n', '<C-k>', api.node.show_info_popup,              opts('Info'))
   vim.keymap.set('n', '<C-r>', api.fs.rename_sub,                     opts('Rename: Omit Filename'))
   vim.keymap.set('n', '<C-t>', api.node.open.tab,                     opts('Open: New Tab'))
-  vim.keymap.set('n', '<C-v>', api.node.open.vertical,                opts('Open: Vertical Split'))
   vim.keymap.set('n', '<C-x>', api.node.open.horizontal,              opts('Open: Horizontal Split'))
   vim.keymap.set('n', '<BS>',  api.node.navigate.parent_close,        opts('Close Directory'))
   vim.keymap.set('n', '<CR>',  api.node.open.edit,                    opts('Open'))
